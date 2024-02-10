6860c477a0af350c1ef4a6d1d677c36d5cc50dee
hlam9 <o3onoobmule@gmail.com>
2024-02-09 23:45:44 -0800

    distributing functions to files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 
 import './App.css';
 import { useState } from 'react';
-
-function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>
-          {value}
-        </button>;
-}
-
+import Square from './Square';
+import calculateWinner from './calculateWinner';
 
 function App() {
   const [xIsNext, setXIsNext] = useState(true);
@@ -60,23 +55,5 @@ function App() {
   );
 
 }
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
 export default App;
