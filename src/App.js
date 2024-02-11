351e3efc22907d41bfb7631221defcc83540c8cd
hlam9 <o3onoobmule@gmail.com>
2024-02-11 00:11:54 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,30 +4,26 @@ import Board from './Board';
 import { useState } from 'react';
 
 function App(){
-  const [xIsNext, setXIsNext] = useState(true);
+  
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
+  
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
+    description = ((move > 0) ? 'Go to move #' + move : 'Go to game start');
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
